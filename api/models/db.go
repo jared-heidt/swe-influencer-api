b0303ec892309869c5e0122139a89cd7449c322a
Jared Heidt <jared14heidt@gmail.com>
2022-09-04 20:29:40 -0400

    completed db.go

---
+++ api/models/db.go
@@ -0,0 +1,58 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var db *sql.DB
+
+type connection struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+}
+
+func (c connection) toString() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		c.Host, c.Port, c.User, c.Password, c.DBName)
+}
+
+func Init() {
+	err := godotenv.Load("config/.env")
+
+	if err != nil {
+		fmt.Println("Error loading .env file: ", err.Error())
+		return
+	}
+
+	connectionInfo := connection{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+
+	db, err = sql.Open(os.Getenv("DB_DRIVER"), connectionInfo.toString())
+
+	if err != nil {
+		fmt.Println("Error connecting to database", err.Error())
+		return
+	}
+
+	fmt.Println("Database is open")
+
+	err = db.Ping()
+
+	if err != nil {
+		fmt.Println("Error pinging database", err.Error())
+	}
+
+	fmt.Println("Database pinged successfully")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jared-heidt/swe-influencer-api
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0
