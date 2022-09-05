c1fe51b6dc97a417d07ddd2e4f75de601675ba01
Jared Heidt <jared14heidt@gmail.com>
2022-09-04 20:42:43 -0400

    added GetAllCreators to Creator.go

--- api/models/Creator.go
+++ api/models/Creator.go
@@ -1,9 +1,47 @@
 package models
 
 type Creator struct {
-	ID         uint32 `gorm:"primary_key; auto_increment" json:"id"`
-	Name       string `gorm:"size:255; not null" json:"name"`
-	Profession string `gorm:"size:255" json:"profession"`
-	Focus      string `gorm:"size:255" json:"focus"`
-	Youtube    string `gorm:"size:255" json:"youtube"`
+	ID         uint32 `json:"id"`
+	Name       string `json:"name"`
+	Profession string `json:"profession"`
+	Focus      string `json:"focus"`
+	Youtube    string `json:"youtube"`
+}
+
+func GetAllCreators() ([]Creator, error) {
+	var creators []Creator
+
+	query := `SELECT * FROM creator`
+
+	rows, err := db.Query(query)
+
+	if err != nil {
+		return creators, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id uint32
+		var name, profession, focus, youtube string
+
+		err := rows.Scan(&id, &name, &profession, &focus, &youtube)
+
+		if err != nil {
+			return creators, err
+		}
+
+		creator := Creator{
+			ID:         id,
+			Name:       name,
+			Profession: profession,
+			Focus:      focus,
+			Youtube:    youtube,
+		}
+
+		creators = append(creators, creator)
+
+	}
+
+	return creators, nil
 }
